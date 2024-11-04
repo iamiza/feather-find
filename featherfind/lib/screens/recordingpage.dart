16083ea7bf13c88588903f15b79205c3d9751b1d
Iza KC <iamizakc@gmail.com>
2024-11-04 21:57:06 +0545

    feature/audio visualization

--- featherfind/lib/screens/recordingpage.dart
+++ featherfind/lib/screens/recordingpage.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:audio_waveforms/audio_waveforms.dart';
 import 'package:featherfind/constants/theme.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +23,8 @@ class Recordingpage extends StatefulWidget {
 }
 
 class _RecordingpageState extends State<Recordingpage> {
+  RecorderController controller = RecorderController();
+  bool isRecording = false;
   Future<String> getFilePath() async {
     final directory = await getTemporaryDirectory();
     final path = '${directory.path}/audio';
@@ -31,54 +34,91 @@ class _RecordingpageState extends State<Recordingpage> {
 
   Future<void> startRecording() async {
     requestPermission();
-    await recorder.openRecorder();
+    //await recorder.openRecorder();
+    setState(() {
+      isRecording = true;
+    });
     String path = await getFilePath();
-    await recorder.startRecorder(toFile: path);
+    await controller.record(path: path);
   }
 
   Future<void> stopRecording() async {
-    await recorder.stopRecorder();
-    await recorder.closeRecorder();
+    // await recorder.stopRecorder();
+    // await recorder.closeRecorder();
+    await controller.stop();
+    setState(() {
+      isRecording = false;
+    });
   }
-  
+
   Future<void> playRecording() async {
     String path = await getFilePath();
 
     final player = FlutterSoundPlayer();
     await player.openPlayer();
-    await player.startPlayer(
-        fromURI: path, codec: Codec.aacADTS);
+    await player.startPlayer(fromURI: path, codec: Codec.aacADTS);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: Column(
         children: [
-          ElevatedButton(
-              onPressed: startRecording, 
-              style: ElevatedButton.styleFrom(
-                backgroundColor: ThemeColor.bottonColor,
-                shape: const CircleBorder(),
-                padding: const EdgeInsets.all(20)
-              ),
+          const SizedBox(height: 80,),
+          //if(isRecording)
+            SizedBox(
+              child: AudioWaveforms(size: const Size(400,250), recorderController: controller,
+              //shouldCalculateScrolledPosition: true,
+              backgroundColor: const Color.fromARGB(255, 231, 228, 228),
+              enableGesture: false,
+              waveStyle: const WaveStyle(
+                showMiddleLine: false,
+                durationStyle: TextStyle(color: Colors.black,fontSize: 16),
+                waveCap: StrokeCap.round,
+                waveColor: Color.fromRGBO(0, 0, 0, 0.5),
+                waveThickness: 3,
+                showDurationLabel: true,
+                durationLinesColor: ThemeColor.bottonColor,
+                spacing: 8.0,
+                showBottom: true,
+                scaleFactor: 50,
+              ),),
               
-              child: const Icon(Icons.mic, color: Colors.white, size: 30,)),
-          const SizedBox(
-            width: 40,
-          ),
-          ElevatedButton(
-              onPressed: stopRecording, 
-              style: ElevatedButton.styleFrom(
-                backgroundColor: ThemeColor.bottonColor,
-                shape: const CircleBorder(),
-                padding: const EdgeInsets.all(20)
-              ),
-              child: const Icon(Icons.stop, color: Colors.white,size: 30,)),
-        ],
-      ),
+            ),
+          const SizedBox(height: 50,),
+          Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: startRecording,
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: ThemeColor.bottonColor,
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(20)),
+                child: const Icon(
+                  Icons.mic,
+                  color: Colors.white,
+                  size: 30,
+                )),
+            const SizedBox(
+              width: 40,
+            ),
+            ElevatedButton(
+                onPressed: stopRecording,
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: ThemeColor.bottonColor,
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(20)),
+                child: const Icon(
+                  Icons.stop,
+                  color: Colors.white,
+                  size: 30,
+                )),
+            //ElevatedButton(onPressed: playRecording, child: const Icon(Icons.play_arrow))
+          ],
+        ),
+      ]),
     ));
   }
 }
